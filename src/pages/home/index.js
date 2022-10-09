7f626006a8abc3d9c8f9e40fb9a7f810b596d42c
angelgt <angel.gamonal@agenciaservitec.com>
2022-10-09 17:37:46 -0500

    update content

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Row from "antd/lib/row";
 import Title from "antd/lib/typography/Title";
 import Text from "antd/lib/typography/Text";
 import Col from "antd/lib/col";
+import { Button } from "../../components/ui";
+import { DrawerUserInformation } from "../../components/pages";
 import { FloatingBubble } from "../../components/ui/FloatingBubble";
 
 export const Home = () => {
+  const [isVisibleDrawerRight, setIsVisibleDrawerRight] = useState(false);
+  console.log("isVisibleDrawerRight->", isVisibleDrawerRight);
+
   return (
     <Row gutter={[16, 16]}>
-      <Col span={24}></Col>
+      <Col span={24}>
+        <Button onClick={() => setIsVisibleDrawerRight(!isVisibleDrawerRight)}>
+          Button
+        </Button>
+        <DrawerUserInformation
+          setIsVisibleDrawerRight={setIsVisibleDrawerRight}
+          isVisibleDrawerRight={isVisibleDrawerRight}
+        />
+        <FloatingBubble />
+      </Col>
       <Col span={24}></Col>
     </Row>
   );
