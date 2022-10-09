fcc4f866801255aecef9f316c9182a4310b1e079
angelgt <angel.gamonal@agenciaservitec.com>
2022-10-09 17:37:22 -0500

    add component DrawerUserInformation

---
+++ src/components/pages/home/DrawerUserInformation.js
@@ -0,0 +1,74 @@
+import { Button, Col, Drawer, Form, Row, Select, Space } from "antd";
+import React from "react";
+import styled from "styled-components";
+import { Input, TextArea } from "../../ui";
+
+export const DrawerUserInformation = ({
+  isVisibleDrawerRight,
+  setIsVisibleDrawerRight,
+}) => {
+  return (
+    <ContainerDrawer
+      title="Informacion de contacto"
+      width={720}
+      onClose={() => setIsVisibleDrawerRight(!isVisibleDrawerRight)}
+      visible={isVisibleDrawerRight}
+      bodyStyle={{
+        paddingBottom: 80,
+      }}
+    >
+      <Form layout="vertical" hideRequiredMark>
+        <Row gutter={[16, 16]}>
+          <Col span={12}>
+            <Input label="Cliente" />
+          </Col>
+          <Col span={12}>
+            <Input label="Dominio" />
+          </Col>
+
+          <Col span={12}>
+            <Input label="Nombres" />
+          </Col>
+          <Col span={12}>
+            <Input label="Apellidos" />
+          </Col>
+
+          <Col span={12}>
+            <Input label="Codigo de pais" />
+          </Col>
+          <Col span={12}>
+            <Input label="Numero" />
+          </Col>
+
+          <Col span={12}>
+            <Input label="Email" />
+          </Col>
+          <Col span={12}>
+            <Input label="Fecha de Creación" />
+          </Col>
+
+          <Col span={24}>
+            <Input label="Asunto" />
+          </Col>
+
+          <Col span={24}>
+            <TextArea />
+          </Col>
+        </Row>
+      </Form>
+    </ContainerDrawer>
+  );
+};
+
+const ContainerDrawer = styled(Drawer)`
+  .site-form-in-drawer-wrapper {
+    position: absolute;
+    right: 0px;
+    bottom: 0px;
+    width: 100%;
+    padding: 10px 16px;
+    text-align: right;
+    background: #fff;
+    border-top: 1px solid #e9e9e9;
+  }
+`;
