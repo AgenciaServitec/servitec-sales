b680ae8f021db4c38894acff139d9f2f00fe1812
noeLMoriano <noel.moriano@webuild.pe>
2022-10-19 23:14:11 -0500

    update PrivateRoute.js

--- src/router/PrivateRoute.js
+++ src/router/PrivateRoute.js
@@ -4,7 +4,5 @@ import { Navigate } from "react-router-dom";
 export const PrivateRoute = ({ path = "/login", children }) => {
   const { authUser } = useAuthentication();
 
-  console.log("authUser->", authUser);
-
   return authUser ? children : Navigate({ to: path });
 };
