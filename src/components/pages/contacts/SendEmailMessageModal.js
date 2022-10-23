783c2295d2cabc69f6e6d2f0612291cf7d22c640
angelgt <angel.gamonal@agenciaservitec.com>
2022-10-23 18:52:30 -0500

    add FetchSendMessage

--- src/components/pages/contacts/SendEmailMessageModal.js
+++ src/components/pages/contacts/SendEmailMessageModal.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Button, Form, Input, Modal, TextArea } from "../../ui";
+import {
+  Button,
+  Form,
+  Input,
+  Modal,
+  modalConfirm,
+  notification,
+  TextArea,
+} from "../../ui";
 import Row from "antd/lib/row";
 import Col from "antd/lib/col";
 import { Controller, useForm } from "react-hook-form";
@@ -7,6 +15,7 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useFormUtils } from "../../../hooks";
 import Typography from "antd/lib/typography";
+import { apiUrl } from "../../../firebase";
 
 const { Text } = Typography;
 
@@ -36,9 +45,39 @@ export const SendEmailMessageModal = ({
     });
   }, [isVisibleSendEmailModal]);
 
-  const onSubmitSendEmail = (formData) => {
-    console.log("formData->", formData);
+  const onSendEmail = async (formData) => {
+    try {
+      setSendingEmailMessage(true);
+      console.log("formData2", formData);
+      const response = await fetchSendEmailMessage(formData);
+
+      if (!response.ok) throw new Error(response.statusText);
+
+      notification({ type: "success", title: "Enviado exitosamente" });
+
+      setSendingEmailMessage(false);
+    } catch (e) {
+      console.error("ErrorSendingEmailMessage:", e);
+      notification({ type: "error" });
+    }
   };
+
+  const fetchSendEmailMessage = async (formData) =>
+    await fetch(`${apiUrl}/send-message`, {
+      method: "POST",
+      headers: {
+        "Access-Control-Allow-Origin": null,
+        "content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+  const onSubmitConfirmSendEmail = (formData) =>
+    modalConfirm({
+      title: "¿Seguro que quieres enviar?",
+      onOK: () => onSendEmail(formData),
+    });
 
   return (
     <Modal
@@ -46,7 +85,7 @@ export const SendEmailMessageModal = ({
       closable={onCLickIsVisibleSendEmailModal}
       onCancel={onCLickIsVisibleSendEmailModal}
     >
-      <Form layout="vertical" onSubmit={handleSubmit(onSubmitSendEmail)}>
+      <Form layout="vertical" onSubmit={handleSubmit(onSubmitConfirmSendEmail)}>
         <Row gutter={[16, 16]}>
           <Col span={24}>
             <Text>Enviar mensaje por email a:</Text>
@@ -95,6 +134,7 @@ export const SendEmailMessageModal = ({
               type="primary"
               size="large"
               disabled={sendingEmailMessage}
+              loading={sendingEmailMessage}
             >
               Enviar
             </Button>
