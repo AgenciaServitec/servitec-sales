45d2fec2e14e19938ad24bbdaa1f58888325427a
angelgt <angel.gamonal@agenciaservitec.com>
2022-10-23 15:12:28 -0500

    add page users

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,4 @@ export * from "./contacts";
 export * from "./contacts/_contactId";
 export * from "./clients";
 export * from "./login";
+export * from "./users";
---
+++ src/pages/users/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Row from "antd/lib/row";
+
+export const Users = () => {
+  return <Row gutter={[16, 16]}>Hola</Row>;
+};
