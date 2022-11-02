28452c9701e7b72f1c8b151a93149d1048897d0d
noelMoriano <nmoriano26@gmail.com>
2022-11-01 23:27:48 -0500

    delete old files ols end points

--- functions/src/api/index.ts
+++ functions/src/api/index.ts
@@ -2,14 +2,7 @@ import express from "express";
 import cors from "cors";
 import { validateRequest, errorHandler } from "./_middlewares";
 import { body } from "express-validator";
-import { PostContact as PostContactMarkoCreativo } from "./markoCreativos";
-import { PostContact as PostContactServitec } from "./servitec";
 import { PostContact as PostContactGeneric } from "./generic";
-import { PostContact as PostContactGamontLlantas } from "./gamont-llantas";
-import { PostContact as PostContactPublicidadGoole } from "./publicidad-google";
-import { PostContact as PostCobiene } from "./cobiene";
-import { PostContact as PostFacilFactura } from "./facil-factura";
-import { PostContact as PostAvcLlantas } from "./avcLlantas";
 import { PostSendMessage } from "./email";
 
 const app: express.Application = express();
@@ -31,90 +24,6 @@ app.post(
   ],
   validateRequest,
   PostContactGeneric
-);
-
-app.post(
-  "/marko-creativos/contact",
-  [
-    body("contact.firstName").exists(),
-    body("contact.lastName").exists(),
-    body("contact.email").exists(),
-    body("contact.phone").exists(),
-  ],
-  validateRequest,
-  PostContactMarkoCreativo
-);
-
-app.post(
-  "/gamont-llantas/contact",
-  [
-    body("contact.firstName").exists(),
-    body("contact.email").exists(),
-    body("contact.phone").exists(),
-  ],
-  validateRequest,
-  PostContactGamontLlantas
-);
-
-app.post(
-  "/publicidad-google/contact",
-  [
-    body("contact.firstName").exists(),
-    body("contact.lastName").exists(),
-    body("contact.email").exists(),
-    body("contact.phone").exists(),
-  ],
-  validateRequest,
-  PostContactPublicidadGoole
-);
-
-app.post(
-  "/servitec/contact",
-  [
-    body("contact.firstName").exists(),
-    body("contact.lastName").exists(),
-    body("contact.phone").exists(),
-    body("contact.email").exists(),
-    body("contact.address").exists(),
-  ],
-  validateRequest,
-  PostContactServitec
-);
-
-app.post(
-  "/cobiene/contact",
-  [
-    body("contact.firstName").exists(),
-    body("contact.lastName").exists(),
-    body("contact.phone").exists(),
-    body("contact.email").exists(),
-  ],
-  validateRequest,
-  PostCobiene
-);
-
-app.post(
-  "/facil-factura/contact",
-  [
-    body("contact.firstName").exists(),
-    body("contact.lastName").exists(),
-    body("contact.phone").exists(),
-    body("contact.email").exists(),
-  ],
-  validateRequest,
-  PostFacilFactura
-);
-
-app.post(
-  "/avc-llantas/contact",
-  [
-    body("contact.firstName").exists(),
-    body("contact.lastName").exists(),
-    body("contact.phone").exists(),
-    body("contact.email").exists(),
-  ],
-  validateRequest,
-  PostAvcLlantas
 );
 
 app.post(
--- functions/src/mailer/facil-factura/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./sendMailContactReceptor";
-export * from "./sendMailContactEmisor";
