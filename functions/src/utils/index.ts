77c3ece6245fb8f2e8c6161d2ec2f89b81cc166c
noelMoriano <nmoriano26@gmail.com>
2023-12-27 20:50:35 -0500

    Fix: send mails and refactoring files

--- functions/src/utils/index.ts
+++ functions/src/utils/index.ts
@@ -1,6 +1,4 @@
 export * from "./utils";
-export * from "./currencyFormat";
-export * from "./encryption";
 export * from "./timeFormat";
 export * from "./abstract";
 export * from "./defaultFirestoreProps";
