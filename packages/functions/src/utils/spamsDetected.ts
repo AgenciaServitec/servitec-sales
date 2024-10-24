f2fe8b959ebef59679b2b8441e166a655949f0e3
PabloAquiseLozano <aquise.pablo@iestpgildaballivian.edu.pe>
2024-10-24 16:30:18 -0500

    Added spamsDetected in emails/contact end point

---
+++ packages/functions/src/utils/spamsDetected.ts
@@ -0,0 +1,25 @@
+import assert from "assert";
+import { fetchCollection } from "../firebase/firestore";
+import { spamsRef } from "../collections";
+import { isEmpty } from "lodash";
+
+export const spamsDetected = async (contact: Contact): Promise<boolean> => {
+  assert(contact.email, "Missing contact.email!");
+  assert(contact.phone.number, "Missing phone.number!");
+
+  const p0 = fetchCollection(
+    spamsRef
+      .where("value", "==", contact.email)
+      .where("isDeleted", "==", false),
+  );
+
+  const p1 = fetchCollection(
+    spamsRef
+      .where("value", "==", contact.phone.number.toString())
+      .where("isDeleted", "==", false),
+  );
+
+  const [spamsWithEmails, spamsWithPhones] = await Promise.all([p0, p1]);
+
+  return !isEmpty([...spamsWithEmails, ...spamsWithPhones]);
+};
