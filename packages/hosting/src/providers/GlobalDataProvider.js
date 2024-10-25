631f5023cd73dc83ad58cdcbc13b975e6134cdad
Joaquin-Huaranga <joaquinhuarangaorihuela@gmail.com>
2024-10-25 18:13:13 -0500

    added spams data

--- packages/hosting/src/providers/GlobalDataProvider.js
+++ packages/hosting/src/providers/GlobalDataProvider.js
@@ -8,11 +8,7 @@ import { orderBy } from "lodash";
 const GlobalDataContext = createContext({
   clients: [],
   users: [],
-  //
-    spams: [],
-    addSpam:()=>{},
-    removeSpam:()=>{},
-  //
+  spams: [],
 });
 
 export const GlobalDataProvider = ({ children }) => {
@@ -30,52 +26,27 @@ export const GlobalDataProvider = ({ children }) => {
       : null
   );
 
-  ///////////////////////////////////////////////////
+  const [spams = [], spamsLoading, spamsError] = useCollectionData(
+    authUser
+      ? firestore.collection("spams").where("isDeleted", "==", false)
+      : null
+  );
 
-  const [spams,setSpams] = useState([])
-
-  const addSpam = async(spamData) =>{
-    try {
-      const spamRef = firestore.collection("spams");
-      const docRef = await spamRef.add({ ...spamData, createAt: new Date() }); // Agrega una fecha de creación
-      setSpams((prevSpams) => [...prevSpams, { id: docRef.id, ...spamData }]); // Incluye el ID en el estado local
-    } catch (error) {
-      notification({ type: "error" });
-    }
-  }
-
-  const removeSpam = async(spamId) =>{
-    console.log(spamId)
-    try {
-      const spamRef = firestore.collection("spams").doc(spamId);
-      await spamRef.delete();
-      setSpams((prevSpams) => prevSpams.filter(spam => spam.id !== spamId));
-    } catch (error) {
-      notification({ type: "error" });
-    }
-  }
-
-  /////////////////////////////////////////////////
-
-  const error = clientsError || usersError;
-  const loading = clientsLoading || usersLoading;
+  const error = clientsError || usersError || spamsError;
+  const loading = clientsLoading || usersLoading || spamsLoading;
 
   useEffect(() => {
     error && notification({ type: "error" });
   }, [error]);
 
-  if (loading) return <Spinner height="100vh" />;
+  if (loading) return <Spinner height="100svh" />;
 
   return (
     <GlobalDataContext.Provider
       value={{
         clients: orderBy(clients, (client) => [client.createAt], ["asc"]),
         users: orderBy(users, (user) => [user.createAt], ["asc"]),
-        ////////////
-        spams: orderBy(spams, (spam) => [spam.createAt], ["asc"]),
-        addSpam,
-        removeSpam,
-        ///////////
+        spams: orderBy(spams, (spam) => [spam.createAt], ["desc"]),
       }}
     >
       {children}
