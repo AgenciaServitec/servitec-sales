77440cf8ce4354f17d10bac0b19b2e2bf2bd5c82
noel <admin@agenciaservitec.com>
2024-12-06 18:32:08 -0500

    Fix: spams detected and phone format

--- packages/functions/src/utils/index.ts
+++ packages/functions/src/utils/index.ts
@@ -3,4 +3,4 @@ export * from "./timeFormat";
 export * from "./abstract";
 export * from "./defaultFirestoreProps";
 export * from "./dateToTimestamp";
-export * from "./spamsDetected";
+export * from "./spamsDetectedAndFormatPhone";
