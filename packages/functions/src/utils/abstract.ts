5ad38629399a49cdea27982c9df4b72e8a4c26a5
Angel Gala <85372974+AngelDev2001@users.noreply.github.com>
2025-01-06 13:48:17 -0500

    Refactor: Order by url (#64)
    * Refactor: Order by url
    
    * Refactor: refactored code
    
    * Add: Added function generic for orderByUrl

--- packages/functions/src/utils/abstract.ts
+++ packages/functions/src/utils/abstract.ts
@@ -1,3 +1,5 @@
+import { orderBy } from "lodash";
+
 export const uniq = (strings: string[]): string[] =>
   strings.map((string) =>
     string
@@ -5,4 +7,13 @@ export const uniq = (strings: string[]): string[] =>
       .toLowerCase()
       .normalize("NFD")
       .replace(/\p{Diacritic}/gu, ""),
+  );
+
+export const orderByUrl = (websites: Web[], status: string) =>
+  orderBy(
+    websites
+      .filter((website) => website.status === status)
+      .map((_website) => _website.url),
+    (value) => value.split("://")[1],
+    "asc",
   );
